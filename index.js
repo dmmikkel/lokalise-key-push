e4b6f140ab01865982da41a7a5b3a50717863f13
Dag Mikkelsen <reversi@gmail.com>
2019-09-26 09:45:48 +0200

    Parse json files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const core = require('@actions/core');
 const glob = require("glob");
 const { LokaliseApi } = require('@lokalise/node-api');
@@ -14,23 +16,39 @@ async function findFiles (pattern) {
   })
 }
 
+async function readJsonFile (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(JSON.parse(data));
+    });
+  })
+}
+
 async function run () {
   try {
     const apiKey = core.getInput('api-token');
     const projectId = core.getInput('project-id');
     const globPattern = core.getInput('glob-pattern');
-    const format = core.getInput('format');
-
-    if (format !== 'json') {
-      throw new Error('Not a supported format');
-    }
+    const keyNameProperty = core.getInput('key-name-property');
 
     const lokalise = new LokaliseApi({ apiKey });
     const files = await findFiles(globPattern);
 
-    const keys = await lokalise.keys.list({ project_id: projectId });
+    const lokaliseKeys = await lokalise.keys.list({ project_id: projectId });
+    const existingKeys = lokaliseKeys.map(x => x.key_name[keyNameProperty]);
+
     console.log('files', files);
-    console.log('keys', keys);
+    console.log('existingKeys', existingKeys);
+
+    files.forEach(file => {
+      const json = await readJsonFile(path.join(process.env.GITHUB_WORKSPACE, file));
+      console.log('File ' + file);
+      console.log(json);
+    })
 
   } catch (error) {
     core.setFailed(error.message);
